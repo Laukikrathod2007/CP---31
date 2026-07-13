bcd12ab0c8e5d653d8aa2b2dae6142ddde422600
Laukikrathod2007 <rathodlaukik184@gmail.com>
2026-07-13 23:34:20 +0530

    Add CP solutions

--- leetcode/1291.cpp
+++ leetcode/1291.cpp
@@ -17,3 +17,28 @@ public:
         return ans;
     }
 };
+
+//Brute force approach
+class Solution {
+public:
+    bool isSequential(int num) {
+        string s = to_string(num);
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] != s[i - 1] + 1)
+                return false;
+        }
+        return true;
+    }
+
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+
+        for (int i = low; i <= high; i++) {
+            if (isSequential(i))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
