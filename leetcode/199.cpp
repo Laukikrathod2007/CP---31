ee06264502c55d0723c8ad1c040d511cca4e19f2
Laukikrathod2007 <rathodlaukik184@gmail.com>
2026-07-14 23:53:32 +0530

    Add CP solutions

---
+++ leetcode/199.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> lvl;
+
+    void dfs(TreeNode* node, int d) {
+        if (!node) return;
+
+        if (d == lvl.size())
+            lvl.push_back({});
+
+        lvl[d].push_back(node->val);
+
+        dfs(node->left, d + 1);
+        dfs(node->right, d + 1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root, 0);
+
+        vector<int> ans;
+
+        for (auto &v : lvl)
+            ans.push_back(v.back());
+
+        return ans;
+    }
+};
